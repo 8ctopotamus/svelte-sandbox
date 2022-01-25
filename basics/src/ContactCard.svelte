9f84d7ea79253c05d8aa5bf6d9ccdc880cd77f73
Josh <zylo.codes@gmail.com>
2022-01-25 08:49:36 -0600

    XSS Attack example

--- basics/src/ContactCard.svelte
+++ basics/src/ContactCard.svelte
@@ -4,9 +4,18 @@
   export let jobTitle
   export let description
   export let imageURL
+
+  let userInput = ''; // fake XSS attack example
+  // Enter this img tag as the Description input value:
+  // <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" onload="alert('You were hacked!');">
+
 </script>
 
 <div class="card">
+
+  <!-- fake XSS attack example -->
+  <input type="text" bind:value={userInput} /> 
+
   <header>
     <div>
       <img src={imageURL} alt={name} />
