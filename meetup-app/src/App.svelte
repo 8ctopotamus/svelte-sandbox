ac66eb513f154207dc2c85eafd0d4478594e8f8a
Josh <zylo.codes@gmail.com>
2022-12-26 13:44:40 -0600

    meetups app started

---
+++ meetup-app/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Header from './UI/Header.svelte'
+  import MeetupItem from './Meetups/MeetupItem.svelte'
+
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description: 'Bla bla bla',
+      imageUrl: 'https://via.placeholder.com/150',
+      address: '27th Nerd Road, 8347 New York',
+      contactEmail: 'code@test.com'
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Learn to swim in 2 hours',
+      description: 'Bla bla bla',
+      imageUrl: 'https://via.placeholder.com/150',
+      address: '27th Nerd Road, 8347 New York',
+      contactEmail: 'code@test.com'
+    }
+  ]
+</script>
+
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
+
+<Header />
+
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem />
+  {/each}
+</section>
