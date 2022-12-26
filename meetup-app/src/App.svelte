a258442c89816a2928843fbb8dbeb9487c1b0b19
Josh <zylo.codes@gmail.com>
2022-12-26 14:05:17 -0600

    MeetupGrid, MeetupItem

--- meetup-app/src/App.svelte
+++ meetup-app/src/App.svelte
@@ -1,39 +1,39 @@
 <script>
   import Header from './UI/Header.svelte'
-  import MeetupItem from './Meetups/MeetupItem.svelte'
+  import MeetupGrid from './Meetups/MeetupGrid.svelte';
 
   const meetups = [
     {
-      id: 'm1',
-      title: 'Coding Bootcamp',
-      subtitle: 'Learn to code in 2 hours',
-      description: 'Bla bla bla',
-      imageUrl: 'https://via.placeholder.com/150',
-      address: '27th Nerd Road, 8347 New York',
-      contactEmail: 'code@test.com'
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description:
+        "In this meetup, we will have some experts that teach you how to code!",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "code@test.com"
     },
     {
-      id: 'm2',
-      title: 'Swim Together',
-      subtitle: 'Learn to swim in 2 hours',
-      description: 'Bla bla bla',
-      imageUrl: 'https://via.placeholder.com/150',
-      address: '27th Nerd Road, 8347 New York',
-      contactEmail: 'code@test.com'
+      id: "m2",
+      title: "Swim Together",
+      subtitle: "Let's go for some swimming",
+      description: "We will simply swim some rounds!",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "swim@test.com"
     }
   ]
 </script>
 
 <style>
-  #meetups {
+  main {
     margin-top: 5rem;
   }
 </style>
 
 <Header />
-
-<section id="meetups">
-  {#each meetups as meetup}
-    <MeetupItem />
-  {/each}
-</section>
+<main>
+  <MeetupGrid {meetups} />
+</main>
