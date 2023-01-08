fc384e3b2671828826e00db055c3399a956cb034
Josh <zylo.codes@gmail.com>
2023-01-08 14:13:13 -0600

    Checkboxes and radio buttons

--- 02-bindings-forms/src/App.svelte
+++ 02-bindings-forms/src/App.svelte
@@ -7,28 +7,34 @@
 	let opt = 1
 	let agreed
 	let favColor = 'green'
+	let favFoods = ["Mac & Cheese"]
 
 	$: console.log(val); 
 	$: console.log(opt);
 	$: console.log(price);
 	$: console.log(agreed);
 	$: console.log(favColor);
+	$: console.log(favFoods);
 </script>
 
+<!-- custom inputs -->
 <CustomInput type="text" bind:val={val} />
 
 <Toggle bind:chosenOption={opt} /> Chosen: {opt}
 
+<!-- automatic number conversion -->
 <input 
 	type="number"
 	bind:value={price}
 />
 
+<!-- boolean checkbox -->
 <label>
 	<input type="checkbox" bind:checked={agreed} />
 	Aggree to terms?
 </label>
 
+<!-- radios (bind:group) -->
 <h2>Favorite color?</h2>
 <label>
 	<input name="color" type="radio" value="red" bind:group={favColor} /> Red
@@ -38,4 +44,16 @@
 </label>
 <label>
 	<input name="color" type="radio" value="blue" bind:group={favColor} /> Blue
+</label>
+
+<!-- Binding multiple checkboxes, returns an array of values -->
+<h2>Favorite foods?</h2>
+<label>
+	<input name="foods" type="checkbox" value="Mac & Cheese" bind:group={favFoods} /> Mac & Cheese
+</label>
+<label>
+	<input name="foods" type="checkbox" value="Sushi" bind:group={favFoods} /> Sushi
+</label>
+<label>
+	<input name="foods" type="checkbox" value="Pad Thai" bind:group={favFoods} /> Pad Thai
 </label>
