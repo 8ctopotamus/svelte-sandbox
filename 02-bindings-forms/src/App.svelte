39df7e19180be012216d2d3c3ac7c7d98dc15c9f
Josh <zylo.codes@gmail.com>
2023-01-08 14:09:58 -0600

    Checkboxes and radio buttons

--- 02-bindings-forms/src/App.svelte
+++ 02-bindings-forms/src/App.svelte
@@ -3,12 +3,39 @@
 	import Toggle from "./components/Toggle.svelte";
 
 	let val = "Test";
+	let price = 0
 	let opt = 1
+	let agreed
+	let favColor = 'green'
 
 	$: console.log(val); 
 	$: console.log(opt);
+	$: console.log(price);
+	$: console.log(agreed);
+	$: console.log(favColor);
 </script>
 
 <CustomInput type="text" bind:val={val} />
 
 <Toggle bind:chosenOption={opt} /> Chosen: {opt}
+
+<input 
+	type="number"
+	bind:value={price}
+/>
+
+<label>
+	<input type="checkbox" bind:checked={agreed} />
+	Aggree to terms?
+</label>
+
+<h2>Favorite color?</h2>
+<label>
+	<input name="color" type="radio" value="red" bind:group={favColor} /> Red
+</label>
+<label>
+	<input name="color" type="radio" value="green" bind:group={favColor} /> Green
+</label>
+<label>
+	<input name="color" type="radio" value="blue" bind:group={favColor} /> Blue
+</label>
